b48e9ef3951b5fd2d81c26575ed0571ded6278f0
Thu Dang <>
2023-03-05 12:49:32 -0600

    add mobile menu

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+// Mobile menu
+const openMenu = document.querySelector(".open-menu");
+const closeMenu = document.querySelector(".close-menu");
+const nav = document.querySelector("#header-nav");
+const overlay = document.querySelector(".overlay");
+function openNav() {
+  overlay.style.height = "100%";
+  openMenu.classList.add("active");
+  closeMenu.classList.add("active");
+  nav.classList.add("active");
+  console.log("working");
+}
+function closeNav() {
+  overlay.style.height = "0%";
+  openMenu.classList.remove("active");
+  closeMenu.classList.remove("active");
+  nav.classList.remove("active");
+}
 // =============Scroll animations
 const scrollElements = document.querySelectorAll(".js-scroll");
 
